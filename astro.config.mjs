4e4b18823132ded63fd1764806cd3a56a3096af7
norbertfarkas-dc <norbert.farkas@dataconsulting.hu>
2026-01-18 20:05:19 +0100

    Add gh pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://https://norbertfarkas-dc.github.io/tetszike/', // <-- GitHub Pages URL
-  base: '/gh-pages/', // <-- ide kell a repo neve
+  site: 'https://norbertfarkas-dc.github.io/tetszike/', // <-- GitHub Pages URL
+  base: '/tetszike/', // <-- ide kell a repo neve
   integrations: [tailwind()],
 });
